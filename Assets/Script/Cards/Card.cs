92943564105408c49c0abb6bea9c4db607084065
Unknown <remi.boscher@gmail.com>
2017-11-18 10:28:52 +0100

    Card base system

---
+++ Assets/Script/Cards/Card.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Card : MonoBehaviour {
+    
+    #region Properties
+    private int cardHealth;
+    #endregion
+
+
+    virtual public void Use() { }
+
+
+    #region Accessors
+    public int CardHealth
+    {
+        get
+        {
+            return cardHealth;
+        }
+
+        set
+        {
+            // Control
+            if (value < 0)
+                Debug.LogError("Card >> Card Health : negative value");
+            
+            // Affectation
+            cardHealth = value;
+
+        }
+    }
+    #endregion
+
+
+
+    public void HandleCardHealth(int _newValue)
+    {
+        cardHealth += _newValue;
+
+        // After effect
+        ChangeShaderMask(cardHealth);
+    }
+
+    public void ChangeShaderMask(int maskIndice)
+    {
+        // Do nothing for now
+    }
+}
---
+++ Assets/Script/Cards/SwordCard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordCard : Card {
+
+    // Override of card use();
+    public override void Use() {
+
+    }
+
+}
