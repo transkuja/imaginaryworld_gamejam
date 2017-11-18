4f20cbcf4e47a18c4ce2efec8b24c96215a4e7b4
Unknown <Sebdami@hotmail.fr>
2017-11-19 00:35:48 +0100

    Add Basket ball passing by from time to time

---
+++ Assets/PlayBallAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBallAnimation : MonoBehaviour {
+    Animator anim;
+    [SerializeField]
+    float timer;
+    float timeToReach;
+    [SerializeField]
+    float minTime = 10.0f;
+    [SerializeField]
+    float maxTime = 30.0f;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        timeToReach = Random.Range(minTime, maxTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if(timer >= timeToReach)
+        {
+            anim.SetTrigger("play");
+            timer = 0.0f;
+            timeToReach = Random.Range(minTime, maxTime);
+        }
+	}
+}
