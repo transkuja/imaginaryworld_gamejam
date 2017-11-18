7ed491e76600b12f96ee66a9f3af185d5d44254d
Unknown <remi.boscher@gmail.com>
2017-11-18 10:37:53 +0100

    PlayerHand and CombinationValues

--- Assets/Script/Cards/Card.cs
+++ Assets/Script/Cards/Card.cs
@@ -4,6 +4,10 @@ public class Card : MonoBehaviour {
     
     #region Properties
     private int cardHealth;
+
+    public int[] combinationValues = new int[2];
+    public const int MIN_COMBINATION_VALUE = 1;
+    public const int MAX_COMBINATION_VALUE = 1;
     #endregion
 
 
